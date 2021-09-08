d64b540aa1f9438718dece055f04d24795bbe632
yuliabcd <yulipuspita280799@gmail.com>
2021-09-08 22:53:16 +0700

    logo

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,8 +3,8 @@ $user = auth()->user();
 @endphp
 <!-- Brand Logo -->
 <a href="{{ route('home') }}" class="brand-link">
-  <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-    class="brand-image img-circle elevation-3" style="opacity: .8">
+  <img src="{{ asset('assets/image/pkk.png') }}" alt="AdminLTE Logo"
+    class="brand-image img-circle elevation-3">
   <span class="brand-text font-weight-light">Siganam</span>
 </a>
 
